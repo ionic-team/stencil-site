40e92ce0581f74ec0836e528ea2bf5764fcfed3c
Max Lynch <max@drifty.com>
2017-07-18 14:14:57 -0500

    hackin and slashin

--- stencil.config.js
+++ stencil.config.js
@@ -2,5 +2,8 @@ exports.config = {
   bundles: [
     { components: ['stencil-site', 'site-header', 'landing-page', 'docs-page', 'demos-page'] },
     { components: ['stencil-router', 'stencil-route', 'stencil-route-link'] }
+  ],
+  collections: [
+    { name: '@stencil/router' }
   ]
 };
