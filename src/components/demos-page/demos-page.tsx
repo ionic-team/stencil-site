820ab4d484af69549cd75b608b47edab8302651c
jgw96 <jgw9617@gmail.com>
2017-08-22 13:07:16 -0500

    chore(demos): add links to more demos

--- src/components/demos-page/demos-page.tsx
+++ src/components/demos-page/demos-page.tsx
@@ -6,17 +6,28 @@ import { Component, State } from '@stencil/core';
 })
 export class DemosPage {
   @State() demos = [
-      {
-        title: 'IonicHN',
-        description: 'Hacker News PWA built with @stencil/core and @ionic/core',
-        url: 'https://corehacker-10883.firebaseapp.com/'
-      },
-      {
-        title: 'StencilNews',
-        description: 'Demo of how you could use @stencil/core to build a fast, modern News PWA',
-        url: 'https://stencilnews.firebaseapp.com/'
-      }
-    ];
+    {
+      title: 'Stenciljs.com',
+      description: 'Yep, this site is also built with Stencil!',
+      url: 'https://github.com/ionic-team/stencil-site'
+    },
+    {
+      title: 'IonicHN',
+      description: 'Hacker News PWA built with @stencil/core and @ionic/core',
+      url: 'https://corehacker-10883.firebaseapp.com/'
+    },
+    {
+      title: 'Stencil Fiber demo',
+      description: 'This showcases the runtime performance of stencil using our async rendering',
+      url: 'https://stencil-fiber-demo.firebaseapp.com/'
+    },
+    {
+      title: 'StencilNews',
+      description: 'Demo of how you could use @stencil/core to build a fast, modern News PWA',
+      url: 'https://stencilnews.firebaseapp.com/'
+    }
+  ];
+  
   render() {
     return (
       <div>
