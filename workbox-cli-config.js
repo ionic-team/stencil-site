107c39f2819f3ba9ea9d4372e0d5af435b979b65
jgw96 <jgw9617@gmail.com>
2017-08-23 10:00:53 -0500

    chore(): change workbox config after consulting with workbox team

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   "skipWaiting": true,
+  "clientsClaim": true,
   "globDirectory": "www/",
   "globPatterns": [
     "**/*.{js,css,json,html,ico,png,svg}"
   ],
   "swDest": "www/sw.js",
   "globIgnores": [
-    "../workbox-cli-config.js",
-    "docs-content/**/*.html"
+    "../workbox-cli-config.js"
   ]
 };
