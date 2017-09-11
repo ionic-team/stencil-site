636acf530d78d8881b219cc02b5194b45ff6f0f9
jgw96 <jgw9617@gmail.com>
2017-09-11 12:42:11 -0500

    chore(): cleanup code

--- src/components/document-component/document-component.tsx
+++ src/components/document-component/document-component.tsx
@@ -1,15 +1,10 @@
-import { Component, Prop, PropDidChange } from '@stencil/core';
+import { Component, Prop } from '@stencil/core';
 @Component({
   tag: 'document-component',
   styleUrl: 'document-component.scss'
 })
 export class DocumentComponent {
   @Prop() pages: string[] = [];
-
-  @PropDidChange('pages')
-  onDocChanged() {
-    // window.scrollTo(0, 0);
-  }
 
   render() {
     return (
