fa69b7d257f31beed05d51481f10161c475c9f11
Max Lynch <max@drifty.com>
2017-09-11 17:11:46 -0500

    Added source to demos

--- src/components/demos-page/demos-page.tsx
+++ src/components/demos-page/demos-page.tsx
@@ -10,22 +10,26 @@ export class DemosPage {
     {
       title: 'Stenciljs.com',
       description: 'Yep, this site is also built with Stencil!',
-      url: 'https://github.com/ionic-team/stencil-site'
+      url: 'https://github.com/ionic-team/stencil-site',
+      source: 'https://github.com/ionic-team/stencil-site',
     },
     {
       title: 'IonicHN',
       description: 'Hacker News PWA built with @stencil/core and @ionic/core',
-      url: 'https://corehacker-10883.firebaseapp.com/'
+      url: 'https://corehacker-10883.firebaseapp.com/',
+      source: 'https://github.com/ionic-team/ionic-stencil-hn-app'
     },
     {
       title: 'Stencil Fiber demo',
       description: 'This showcases the runtime performance of stencil using our async rendering',
-      url: 'https://stencil-fiber-demo.firebaseapp.com/'
+      url: 'https://stencil-fiber-demo.firebaseapp.com/',
+      source: 'https://github.com/ionic-team/stencil-fiber-demo'
     },
     {
       title: 'StencilNews',
       description: 'Demo of how you could use @stencil/core to build a fast, modern News PWA',
-      url: 'https://stencilnews.firebaseapp.com/'
+      url: 'https://stencilnews.firebaseapp.com/',
+      source: 'https://github.com/ionic-team/stencil-news'
     }
   ];
 
@@ -42,7 +46,11 @@ export class DemosPage {
           return [
             <h4>{demo.title}</h4>,
             <p>{demo.description}</p>,
-            <p><a target="_blank" rel="noopener" href={demo.url}>Demo</a></p>
+            <p>
+              <a target="_blank" rel="noopener" href={demo.url}>Demo</a>
+              &nbsp;&nbsp;
+              <a target="_blank" rel="noopener" href={demo.source}>Source</a>
+            </p>
           ];
         })}
       </div>
