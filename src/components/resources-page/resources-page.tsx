c3fd620f5ef67c776610d506aa98bab350f014c6
Max Lynch <max@drifty.com>
2017-09-18 17:44:54 -0500

    More links

--- src/components/resources-page/resources-page.tsx
+++ src/components/resources-page/resources-page.tsx
@@ -5,6 +5,20 @@ import { Component } from '@stencil/core';
   styleUrl: 'resources-page.scss'
 })
 export class ResourcesPage {
+  LINKS = {
+    TEMPLATES: [
+      { title: 'Official Stencil Starter Project', url: 'https://github.com/ionic-team/stencil-starter' },
+      { title: 'Stencil Boilerplate with Server Side Rendering', url: 'https://github.com/mitchellsimoens/stencil-boilerplate' },
+      { title: 'Angular Stencil: use Stencil-built components in Angular', url: 'https://github.com/seveves/angular-stencil' }
+    ],
+    COMPONENTS: [
+      { title: 'Stencil Card Component', url: 'https://github.com/henriquecustodia/stencil-card-app' },
+      { title: 'st-image: lazy loaded images', url: 'https://github.com/jgw96/st-img' },
+      { title: 'st-payment: Stencil Payment API Component', url: 'https://github.com/Fdom92/stencil-payment' },
+      { title: 'st-fetch: A simple component for performing http fetch calls', url: 'https://github.com/Fdom92/stencil-fetch' }
+      //{ title: '', url: '' }
+    ]
+  }
   constructor() {
     document.title = `Stencil Resources`;
   }
@@ -35,7 +49,9 @@ export class ResourcesPage {
             inaccurate, or outdated information and code snippets.
           </p>
           <ul>
+            {/* TODO: Turn these into an auto array map to use science to our benefit */}
             <li><a target="_blank" href="https://www.youtube.com/watch?v=UfD-k7aHkQE">Announcing Stencil.js</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=MqMYaT1GlWY">Stencil - Getting Started (video)</a></li>
             <li><a target="_blank" href="https://github.com/ospatil/ng-components-integration">Using a Stencil-built component in Angular</a></li>
             <li><a target="_blank" href="https://coryrylan.com/blog/create-your-first-web-component-with-stencil-js">Create your First Stencil Component</a></li>
             <li><a target="_blank" href="https://alligator.io/stencil/getting-started/">Getting Started With Stencil</a></li>
@@ -44,6 +60,17 @@ export class ResourcesPage {
             <li><a target="_blank" href="https://www.datacodedesign.de/webkomponenten-mit-stencil-ein-erster-ueberblick/">Webkomponenten mit Stencil – Ein erster Überblick (in German)</a></li>
             <li><a target="_blank" href="https://medium.com/t%C3%BCrkiye/stencile-giri%C5%9F-41e90e37595d">Stencil’e Giriş (in Turkish)</a></li>
             <li><a target="_blank" href="https://medium.com/t%C3%BCrkiye/stencilde-bilesenler-arasi-haberlesme-52523a470fa9">Stencil’de Bileşenler Arası Haberleşme (in Turkish)</a></li>
+          </ul>
+        </div>
+        <div>
+          <h2>Third-party Components/Templates</h2>
+          <ul>
+          {this.LINKS.COMPONENTS.map(link => {
+            return (<li><a target="_blank" href={link.url}>{link.title}</a></li>);
+          })}
+          {this.LINKS.TEMPLATES.map(link => {
+            return (<li><a target="_blank" href={link.url}>{link.title}</a></li>);
+          })}
           </ul>
         </div>
       </div>
