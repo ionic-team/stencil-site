969ab5cd1eb609cb5d265c7c1d9cd3a1a5c5344a
jgw96 <jgw9617@gmail.com>
2018-01-05 14:16:51 -0600

    docs(PWAs): beginning of update for PWA push

--- src/components/pwas-page/pwas-page.tsx
+++ src/components/pwas-page/pwas-page.tsx
@@ -25,8 +25,8 @@ export class pwasPage {
 
           <ul>
             <li>
-              Run <code>git clone https://github.com/ionic-team/stencil-app-starter.git my-pwa</code> in your terminal.
-          </li>
+              Run <code>git clone https://github.com/ionic-team/ionic-pwa-toolkit.git my-pwa</code> in your terminal.
+            </li>
             <li>
               Run <code>npm run build</code>
             </li>
@@ -36,22 +36,38 @@ export class pwasPage {
 
         <img src="/assets/img/pwa.png"></img>
 
-          <h4>Under the hood</h4>
+          <h4>What does this include?</h4>
+          <ul>
+            <li>Stencil</li>
+            <li>Ionic</li>
+            <li>Stencil Router</li>
+            <li>Web Push notifications setup</li>
+            <li>Unit Test setup</li>
+            <li>Pre-Rendering</li>
+            <li>Zero config lazy loading</li>
+            <li>Zero config code splitting</li>
+            <li>Polyfills conditionally loaded based on browser support</li>
+            <li>ES6 by default for new browsers, ES5 for older browesrs</li>
+            <li>Everything needed for an add to homscreen PWA (service worker, web manifest, meta tags for iOS)</li>
+            <li>lazy-img component for lazy loading below the fold images</li>
+          </ul>
+
+          <h4>Key Pieces</h4>
 
           <h5>Service Worker</h5>
           <p>
             When your run <code>npm run build</code> we automatically generate a Service Worker for you using <a href="https://workboxjs.org/">Workbox</a> that handles pre-caching your assets.
-        </p>
+          </p>
 
           <stencil-route-link url="/docs/service-workers">
             Read more about Service Workers here.
-        </stencil-route-link>
+          </stencil-route-link>
 
           <h5>Web Manifest</h5>
           <p>
             By default we include a Web Manifest that has all the neccessary entries to get the Add to Homescreen prompt. You can see that
-          web manifest <a href="https://github.com/ionic-team/stencil-app-starter/blob/master/src/manifest.json">here</a>.
-        </p>
+            web manifest <a href="https://github.com/ionic-team/stencil-app-starter/blob/master/src/manifest.json">here</a>.
+          </p>
 
           <h4>PWAs built with Stencil</h4>
 
