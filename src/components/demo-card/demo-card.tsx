2e6bc979a8420a6a3b6d6e93937b8b2c2ca04d8b
Kevin Ports <kevin.ports@gmail.com>
2018-04-09 09:45:42 -0500

    Add new demo card component for demos page

--- src/components/demo-card/demo-card.tsx
+++ src/components/demo-card/demo-card.tsx
@@ -5,7 +5,7 @@ import { Component, Prop} from '@stencil/core';
   styleUrl: 'demo-card.scss'
 })
 export class DemoCard {
-  
+
   @Prop() title: string;
   @Prop() description: string;
   @Prop() imgPath: string;
@@ -17,15 +17,15 @@ export class DemoCard {
       <div>
         <div class="demo-card__image">
           <a target="_blank" rel="noopener" href={this.demoUrl}>
-            <img src={this.imgPath} />
+            <img src={`${this.imgPath}.jpg`} srcSet={`${this.imgPath}.jpg 1x, ${this.imgPath}@2x.jpg 2x`} />
           </a>
         </div>
         <h4>{this.title}</h4>
         <p>{this.description}</p>
         <p>
-          <a target="_blank" rel="noopener" href={this.demoUrl}>Demo</a>
+          <a target="_blank" rel="noopener" href={this.demoUrl} class="text-link text-link--primary">Demo</a>
           &nbsp;&nbsp;
-          <a target="_blank" rel="noopener" href={this.sourceUrl}>Source</a>
+          <a target="_blank" rel="noopener" href={this.sourceUrl} class="text-link text-link--secondary">Source</a>
         </p>
       </div>
     );
