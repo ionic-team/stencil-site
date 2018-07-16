1dd34fda6ec65e0100d4b3a026ed1bd73d444fa9
Dan Dascalescu <ddascalescu+github@gmail.com>
2018-07-15 19:49:36 -0700

    Smallest PR ever

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -81,7 +81,7 @@ export class LandingPage {
         <section id="three-points">
           <div class="point-card simple">
             <h2>Simple</h2>
-            <p>With intentionally small tooling,a tiny API, zero configuration, and TypeScript support, you're set.</p>
+            <p>With intentionally small tooling, a tiny API, zero configuration, and TypeScript support, you're set.</p>
           </div>
 
           <div class="point-card performant">
