199a128a6b71ece2bc7c913950c8694fe8dfe037
Max Lynch <max@drifty.com>
2019-04-24 13:23:38 -0500

    Bindings language

--- src/components/ds-page/ds-page.tsx
+++ src/components/ds-page/ds-page.tsx
@@ -70,7 +70,7 @@ export class DSPage {
             <h4>Custom pricing</h4>
             <ul>
               <li><i>Everything in Stencil OSS +</i></li>
-              <li><a href="#bindings">Generate bindings for Angular, React, and Vue</a></li>
+              <li><a href="#libraries">Generate libraries for Angular, React, and Vue</a></li>
               <li><a href="#documentation">Automated Documentation generation</a></li>
               <li><a href="#testing">Visual Regression testing tools</a></li>
               <li><a href="#accessibility">Accessibility verification tools</a></li>
@@ -94,8 +94,8 @@ export class DSPage {
               helping teams be successful with their Design System initiatives.
             </p>
           </li>
-          <li id="bindings">
-            <b>Why do Angular, React, and Vue need bindings?</b>
+          <li id="libraries">
+            <b>What are Angular, React, and Vue libraries for Stencil?</b>
             <p>
               Stencil builds standard Web Components that run in all modern browsers, with intelligent loading of any 
               necessary polyfills for clients with missing APIs.
