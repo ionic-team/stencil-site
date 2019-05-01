60014af124abb46f2572296160569329efd65894
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-05-01 23:56:27 +0200

    chore(): update stencil

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,10 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   outputTargets: [
-    { type: 'www' },
+    {
+      type: 'www',
+      baseUrl: 'https://next.stenciljs.com'
+    },
     {
       type: 'dist-hydrate-script',
       dir: 'dist/prerender'
