f1823df5b0002860aaf028a5655e99f70b259065
Kevin Ports <kevin.ports@gmail.com>
2019-06-06 09:52:26 -0500

    Hide blog

--- src/components/site-header/site-header.tsx
+++ src/components/site-header/site-header.tsx
@@ -67,10 +67,12 @@ export class SiteHeader {
             Resources
           </stencil-route-link>
 
+          {/*
           <stencil-route-link url="/blog" exact={true} onClick={() => { this.hideNav() }}>
             Blog
           </stencil-route-link>
-
+          */}
+          
           <stencil-route-link url="/design-systems" exact={true} onClick={() => { this.hideNav() }} class="stencil-ds">
             <svg width="21" height="14" viewBox="0 0 21 14" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.77264 4.60664L8.95159 1H13.9671L10.7921 4.60664H5.77264ZM4.42225 4.62637L8.5 0H16.1797L12.107 4.62637H20.25L16.0855 9.37363H16.0791L12 14H4.33337L8.37826 9.37363H0.25L4.41447 4.62637H4.42225ZM4.86748 5.62637L2.45748 8.37364H15.6325L18.0425 5.62637H4.86748ZM7.96391 11.3668L6.536 13H11.5485L14.6914 9.43542L7.96391 11.3668Z"/></svg>
             DS
