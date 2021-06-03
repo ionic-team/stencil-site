1bc453ba35b74bf39f9b28b1a704bb91a96fdc6c
perry <perry@ionic.io>
2021-06-03 14:15:11 -0500

    homepage & color tweaks

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -32,10 +32,9 @@ export class LandingPage {
             Stencil Enterprise Webinar: Design Systems at Scale
           </feature-pill>
           <hgroup>
-            <h1>Build. Distribute. Adopt. </h1>
+            <h1>Build. Customize. Distribute. Adopt.</h1>
             <p>
-              Stencil is a toolchain for building reusable, scalable Design Systems. Generate small, blazing fast, and 100% standards based Web Components that run in every
-              browser.
+              Stencil is a toolchain for building reusable, scalable Design Systems. Generate small, blazing fast, and 100% standards based Web Components that run in every browser.
             </p>
             <stencil-route-link url="/docs/getting-started" class="hero-button" anchorClass="btn-get-started">
               Get started
