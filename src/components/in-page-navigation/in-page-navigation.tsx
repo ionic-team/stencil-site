1e141265363f15c456b4027dbe88a85aab4b498d
jaredcbaum <jaredcbaum@gmail.com>
2021-08-06 08:42:11 -0500

    fix(in-page-navigation): Better section highlighting

--- src/components/in-page-navigation/in-page-navigation.tsx
+++ src/components/in-page-navigation/in-page-navigation.tsx
@@ -2,16 +2,15 @@ import { Component, Prop, ComponentInterface, Listen, State, Watch, h } from '@s
 import { MarkdownHeading } from '../../global/definitions';
 
 interface ItemOffset {
-  id: string,
-  topOffset: number
+  id: string;
+  topOffset: number;
 }
 
 @Component({
   tag: 'in-page-navigation',
-  styleUrl: 'in-page-navigation.css'
+  styleUrl: 'in-page-navigation.css',
 })
 export class InPageNavigtion implements ComponentInterface {
-
   @Prop() pageLinks: MarkdownHeading[] = [];
   @Prop() srcUrl: string = '';
   @Prop() currentPageUrl: string = '';
@@ -20,26 +19,25 @@ export class InPageNavigtion implements ComponentInterface {
 
   @Listen('scroll', { target: 'window' })
   function() {
-    const itemIndex = this.itemOffsets.findIndex(item => item.topOffset > window.scrollY);
+    const itemIndex = this.itemOffsets.findIndex(item => item.topOffset > window.scrollY + 1);
     if (itemIndex === 0) {
       this.selectedId = undefined;
     } else if (itemIndex === -1) {
-      this.selectedId = this.itemOffsets[this.itemOffsets.length - 1].id
+      this.selectedId = this.itemOffsets[this.itemOffsets.length - 1].id;
     } else {
-      this.selectedId = this.itemOffsets[itemIndex - 1].id
+      this.selectedId = this.itemOffsets[itemIndex - 1].id;
     }
   }
-
 
   @Watch('pageLinks')
   @Listen('resize', { target: 'window' })
   updateItemOffsets() {
     requestAnimationFrame(() => {
-      this.itemOffsets = this.pageLinks.map((pl) => {
+      this.itemOffsets = this.pageLinks.map(pl => {
         const item = document.getElementById(pl.id)!;
         return {
           id: pl.id,
-          topOffset: item.getBoundingClientRect().top + window.scrollY
+          topOffset: item.getBoundingClientRect().top + window.scrollY,
         };
       });
     });
@@ -48,7 +46,6 @@ export class InPageNavigtion implements ComponentInterface {
   componentDidLoad() {
     this.updateItemOffsets();
   }
-
 
   cleanHeader(str: string) {
     // The Markdown headers can contain HTML escaped characters such as "&#39;" (')
@@ -62,35 +59,33 @@ export class InPageNavigtion implements ComponentInterface {
   render() {
     const pageLinks = this.pageLinks.filter(pl => pl.level !== 1);
     const submitEditLink = (
-       <a class="submit-edit-link" href={`https://github.com/ionic-team/stencil-site/edit/master/${this.srcUrl}`}>
-         <app-icon name="github"></app-icon>
-         <span>Submit an edit</span>
-       </a>
+      <a class="submit-edit-link" href={`https://github.com/ionic-team/stencil-site/edit/master/${this.srcUrl}`}>
+        <app-icon name="github"></app-icon>
+        <span>Submit an edit</span>
+      </a>
     );
 
     if (pageLinks.length === 0) {
-      return (
-        <div>
-          { submitEditLink }
-        </div>
-      );
+      return <div>{submitEditLink}</div>;
     }
 
     return (
       <div>
         <h5>Contents</h5>
         <ul class="heading-links">
-          { pageLinks.map(pl => (
-          <li class={{
-              'heading-link': true,
-              [`size-h${pl.level}`]: true,
-              'selected': this.selectedId === pl.id
-            }}>
-            <stencil-route-link url={`${this.currentPageUrl}#${pl.id}`}>{this.cleanHeader(pl.text)}</stencil-route-link>
-          </li>
-          )) }
+          {pageLinks.map(pl => (
+            <li
+              class={{
+                'heading-link': true,
+                [`size-h${pl.level}`]: true,
+                'selected': this.selectedId === pl.id,
+              }}
+            >
+              <stencil-route-link url={`${this.currentPageUrl}#${pl.id}`}>{this.cleanHeader(pl.text)}</stencil-route-link>
+            </li>
+          ))}
         </ul>
-        { submitEditLink }
+        {submitEditLink}
       </div>
     );
   }
