38f40d3bca0b524b67d7430dcf897e8bd97e8ba4
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2021-09-01 18:43:47 +0200

    chore: that repository doesn't exist anymore
    and there wasn't a replacement found during a quick research

--- src/components/resources-page/resources-data.ts
+++ src/components/resources-page/resources-data.ts
@@ -13,7 +13,6 @@ export const RESOURCES = {
     { title: 'st-payment: Stencil Payment API Component', url: 'https://github.com/Fdom92/stencil-payment' },
     { title: 'st-fetch: A simple component for performing http fetch calls', url: 'https://github.com/Fdom92/stencil-fetch' },
     { title: 'web-photo-filter: Use webGL for amazing instagram like filters', url: 'https://github.com/peterpeterparker/web-photo-filter' },
-    { title: 'stencil-flip-images: Awesome animated image gallery', url: 'https://github.com/jepiqueau/stencil-flip-images' },
     { title: 'd3-stencil: Charts built with D3 and Stencil. Framework-agnostic, simple.', url: 'https://d3-stencil.dev' },
     { title: 'Animatable: Animate any HTML Element using Web Animations API in a declarative way! 💅', url: 'https://proyecto26.github.io/animatable-component' },
     { title: 'IonPhaser: A web component to integrate Phaser Framework with Angular, React, Vue, etc 🎮', url: 'https://github.com/proyecto26/ion-phaser' },
