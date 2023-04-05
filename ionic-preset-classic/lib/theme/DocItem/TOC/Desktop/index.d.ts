ac77a7cdd625c116ea303bdaa87c4abe441d6a0a
jared <jaredcbaum@gmail.com>
2023-04-05 10:41:35 -0600

    refactor(): Abstract docusaurus preset to npm

--- ionic-preset-classic/lib/theme/DocItem/TOC/Desktop/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-/// <reference types="@docusaurus/theme-classic" />
-import type DesktopType from '@theme/DocItem/TOC/Desktop';
-import type { WrapperProps } from '@docusaurus/types';
-type Props = WrapperProps<typeof DesktopType>;
-export default function DesktopWrapper(props: Props): JSX.Element;
-export {};
