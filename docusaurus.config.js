2f7ca93520549b12326d1a399b8e5c76b137ee08
jared <jaredcbaum@gmail.com>
2023-11-08 11:59:51 -0700

    chore: hook up to new theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,8 +33,8 @@ const config = {
 
   presets: [
     [
-      '@ionic-internal/preset-classic',
-      /** @type {import('@ionic-internal/preset-classic').Options} */
+      '@ionic-docs/preset-classic',
+      /** @type {import('@ionic-docs/preset-classic').Options} */
       ({
         docs: {
           routeBasePath: '/',
@@ -47,7 +47,7 @@ const config = {
 
             const EXCLUDE_TOP_LEVEL_IDS = ['build-variables', 'telemetry'];
 
-            // remove the items in `EXCLUDE_TOP_LEVEL_IDS`, which may exist in 1+ subdirectories for versioned docs
+            // remove the items in `EXCLUDE_TOP_LEVEL_IDS`, which may exist in 1+  subdirectories for versioned docs
             // note: this removes any `EXCLUDE_TOP_LEVEL_IDS` entry for _all_ versioned docs and the top level `./docs`
             const filteredSidebar = defaultSidebar.filter(
               (item) => !EXCLUDE_TOP_LEVEL_IDS.find((toExclude) => item.id?.match(`.*${toExclude}$`)),
@@ -66,36 +66,24 @@ const config = {
     ],
   ],
 
-  plugins: [
-    [
-      'docusaurus-plugin-module-alias',
-      {
-        alias: {
-          react: path.resolve(__dirname, './node_modules/react'),
-          'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+  themeConfig:
+    /** @type {import('@ionic-docs/preset-classic').ThemeConfig} */
+    ({
+      logo: {
+        alt: 'Site Logo',
+        src: `/img/logo-light.png`,
+        srcDark: `/img/logo-dark.png`,
+        href: `https://${HOSTNAME}`,
+        target: '_self',
+        width: 96,
+        height: 20,
+        after: {
+          html: `<a href="/docs"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="14" fill="none"><path fill="#92A0B3" d="M4.53 13C8.69 13 11 10.52 11 6.96 11 3.42 8.7.93 4.53.93H.43V13h4.1Zm4.3-6.04c0 2.5-1.4 4.14-4.37 4.14H2.48V2.83h1.98c2.97 0 4.37 1.64 4.37 4.13Zm5.9 0c0-2.6 1.7-4.31 4.06-4.31 2.36 0 4.06 1.71 4.06 4.31 0 2.6-1.7 4.32-4.06 4.32-2.37 0-4.07-1.71-4.07-4.31Zm-2.18 0a6.06 6.06 0 0 0 6.24 6.3c3.67 0 6.24-2.7 6.24-6.3A6.06 6.06 0 0 0 18.79.67a6.06 6.06 0 0 0-6.24 6.3Zm14.01-.01c0 3.72 2.52 6.29 6.03 6.29 3 0 5.3-1.79 5.8-4.66h-2.2c-.4 1.7-1.78 2.69-3.6 2.69-2.4 0-3.85-1.72-3.85-4.32 0-2.6 1.44-4.3 3.85-4.3 1.82 0 3.2.98 3.6 2.68h2.2C37.9 2.46 35.6.67 32.6.67c-3.51 0-6.03 2.57-6.03 6.28Zm17.79 4.43c-1.63 0-2.5-.86-2.59-2.15h-2.04c.07 1.97 1.31 4 4.52 4 2.9 0 4.8-1.42 4.8-3.61 0-4.83-6.8-2.77-6.8-5.43 0-1.07.87-1.66 2.28-1.66 1.54 0 2.34.95 2.36 2.14h2.02c-.05-2.26-1.54-4-4.38-4-2.69 0-4.32 1.65-4.32 3.58 0 4.82 6.75 2.51 6.75 5.45 0 1.09-.95 1.69-2.6 1.69Z"/></svg></a>`,
         },
       },
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@ionic-internal/preset-classic').ThemeConfig} */
-    ({
       navbar: {
         hideOnScroll: false,
-        logo: {
-          alt: 'Stencil Logo',
-          src: 'img/logo-dark.svg',
-          srcDark: 'img/logo-light.svg',
-          href: `https://${HOSTNAME}`,
-          target: '_self',
-        },
         items: [
-          {
-            type: 'docsVersionDropdown',
-            position: 'right',
-            dropdownActiveClassDisabled: true,
-          },
           {
             type: 'search',
             position: 'right',
@@ -110,7 +98,7 @@ const config = {
           {
             type: 'html',
             position: 'right',
-            value: '<div class="navbar__separator"></div>',
+            value: '<div class="separator"></div>',
           },
           {
             href: 'https://github.com/stencil-community',
@@ -125,27 +113,84 @@ const config = {
           {
             type: 'html',
             position: 'right',
-            value: '<div class="navbar__separator"></div>',
+            value: '<div class="separator"></div>',
           },
           {
-            type: 'html',
+            type: 'localeDropdown',
             position: 'right',
-            value:
-              '<a href="https://stenciljs.jp/docs/introduction" target="_blank" rel="noopener" class="navbar__icon translation"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" fill="none"><path stroke="#03060B" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1.5 3h12.12M7.56 1v2M10.92 17l4.04-9.33L19 17M12.16 14.33h5.6M11.31 3S10.3 6.92 7.85 9.88c-2.44 2.95-5 4.45-5 4.45"/><path stroke="#03060B" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.25 12.33s-1.47-1.12-3.03-3.12c-1.56-2-2.35-3.54-2.35-3.54"/></svg></a>',
+            dropdownItemsAfter: [
+              {
+                to: 'https://stenciljs.jp/docs/introduction',
+                label: '日本語',
+                target: '_blank',
+                rel: null,
+              },
+            ],
+            className: 'icon-link language navbar__item',
           },
           {
-            type: 'html',
+            href: 'https://twitter.com/stenciljs',
             position: 'right',
-            value:
-              '<a href="https://twitter.com/stenciljs" target="_blank" rel="noopener" class="navbar__icon"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" fill="none"><path fill="#03060B" d="M18 1.9c-.68.32-1.39.54-2.12.63A4 4 0 0 0 17.5.3c-.72.47-1.52.8-2.34.98-.35-.4-.77-.73-1.23-.95A3.4 3.4 0 0 0 12.46 0c-2.04 0-3.7 1.8-3.7 4.04 0 .3.04.62.1.92a9.7 9.7 0 0 1-4.2-1.22 10.8 10.8 0 0 1-3.4-3c-.34.61-.5 1.31-.51 2.03 0 1.4.66 2.63 1.65 3.36a3.38 3.38 0 0 1-1.68-.5v.04a3.97 3.97 0 0 0 2.96 3.96 3.42 3.42 0 0 1-1.66.07 3.76 3.76 0 0 0 3.45 2.8A7.01 7.01 0 0 1 0 14.18 9.7 9.7 0 0 0 5.65 16c6.8 0 10.52-6.15 10.52-11.5L16.16 4A7.94 7.94 0 0 0 18 1.89Z"/></svg></a>',
+            className: 'icon-link icon-link-mask icon-link-twitter',
+            'aria-label': 'Twitter',
+            target: '_blank',
           },
           {
-            type: 'html',
+            href: 'https://ionic.link/discord',
             position: 'right',
-            value:
-              '<a href="https://github.com/ionic-team/stencil" target="_blank" rel="noopener" class="navbar__icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none"><path fill="#03060B" d="M8 0a8.1 8.1 0 0 0-8 8.2c0 3.63 2.3 6.7 5.47 7.79l.14.01c.3 0 .4-.22.4-.4v-1.4c-.3.06-.57.1-.81.1-1.54 0-1.89-1.2-1.89-1.2-.36-.95-.89-1.2-.89-1.2-.7-.5 0-.5.05-.5.8.06 1.23.84 1.23.84.4.7.94.9 1.41.9.38 0 .72-.12.92-.21.07-.53.28-.9.5-1.1-1.77-.2-3.64-.91-3.64-4.05 0-.9.31-1.63.82-2.2-.08-.21-.35-1.05.08-2.18l.18-.01c.3 0 .94.1 2.02.86a7.5 7.5 0 0 1 4.01 0c1.08-.75 1.73-.86 2.02-.86l.18.01c.44 1.13.16 1.97.08 2.18.5.57.82 1.3.82 2.2 0 3.15-1.87 3.84-3.65 4.04.28.25.54.75.54 1.52l-.01 2.25c0 .2.1.41.4.41l.15-.01A8.19 8.19 0 0 0 16 8.2 8.1 8.1 0 0 0 8 0Z"/></svg></a>',
+            className: 'icon-link icon-link-mask icon-link-discord',
+            'aria-label': 'Discord',
+            target: '_blank',
+          },
+          {
+            href: 'https://github.com/ionic-team/stencil',
+            position: 'right',
+            className: 'icon-link icon-link-mask icon-link-github',
+            'aria-label': 'GitHub repository',
+            target: '_blank',
           },
         ],
+      },
+      sidebar: {
+        versionDropdown: {},
+        productDropdown: {
+          title: 'Stencil Docs',
+          logo: {
+            width: 20,
+            height: 20,
+            alt: 'Stencil Logo',
+            src: 'img/components/product-dropdown/logo-dark.png',
+            srcDark: 'img/components/product-dropdown/logo-light.png',
+          },
+          textLinks: [
+            {
+              url: {
+                href: 'https://forum.ionicframework.com/c/stencil/21',
+                target: '_blank',
+                rel: 'noopener nofollow',
+              },
+              label: 'Forum',
+            },
+          ],
+          iconLinks: [
+            {
+              key: 'github',
+              url: {
+                href: 'https://github.com/ionic-team/stencil',
+                target: '_blank',
+                rel: 'noopener nofollow',
+              },
+            },
+            {
+              key: 'twitter',
+              url: {
+                href: 'https://twitter.com/stenciljs',
+                target: '_blank',
+                rel: 'noopener nofollow',
+              },
+            },
+          ],
+        },
       },
       prism: {
         theme: themes.github,
